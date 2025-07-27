3e99743b5c8e33d8c5e9dba184ec06704ebb7922
Samuel Massé <samuelmasse4@gmail.com>
2025-07-27 15:37:04 -0700

    fix

--- src/AlvorEngine/RootScale.cs
+++ src/AlvorEngine/RootScale.cs
@@ -6,7 +6,7 @@ public class RootScale(RootScreen screen)
     private int numerator = (int)Math.Round(screen.MonitorScale * 4);
     private int denominator = 4;
 
-    public ref int Numerator => ref numerator; 
+    public ref int Numerator => ref numerator;
     public ref int Denominator => ref denominator;
 
     public float Scale => Numerator / (float)Denominator;
