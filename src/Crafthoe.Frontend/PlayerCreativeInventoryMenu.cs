e5f6ec07aba62798ba56f772b92418e7d071791a
Samuel Massé <samuelmasse4@gmail.com>
2025-09-05 21:21:40 -0700

    inventory

--- src/Crafthoe.Frontend/PlayerCreativeInventoryMenu.cs
+++ src/Crafthoe.Frontend/PlayerCreativeInventoryMenu.cs
@@ -10,28 +10,52 @@ public class PlayerCreativeInventoryMenu(AppStyle s)
         Node(menu, out var vert)
             .SizeInnerSumRelativeV((0, 1))
             .SizeInnerMaxRelativeV((1, 0))
-            .PaddingV((16, 16, 16, 16))
-            .InnerSpacingV(16)
+            .PaddingV((32, 32, 32, 32))
+            .InnerSpacingV(32)
             .ColorV((1, 0, 0, 1))
             .IsSelectableV(true)
             .InnerLayoutV(InnerLayout.VerticalList)
             .AlignmentV(Alignment.Center);
+
+        Node(vert, out var title)
+            .Mut(s.Label)
+            .TextV("Building Blocks");
+
+        Node(vert, out var blocksVert)
+            .SizeInnerSumRelativeV((0, 1))
+            .SizeInnerMaxRelativeV((1, 0))
+            .InnerSpacingV(12)
+            .InnerLayoutV(InnerLayout.VerticalList);
         {
             for (int y = 0; y < 5; y++)
             {
-                Node(vert, out var hor)
+                Node(blocksVert, out var blocksHor)
                     .SizeInnerSumRelativeV((1, 0))
                     .SizeInnerMaxRelativeV((0, 1))
-                    .InnerSpacingV(16)
+                    .InnerSpacingV(12)
                     .InnerLayoutV(InnerLayout.HorizontalList);
                 {
                     for (int x = 0; x < HotBarSlots.Count; x++)
                     {
-                        Node(hor, out var square)
+                        Node(blocksHor, out var square)
                             .Mut(s.Button)
                             .SizeV((120, 120));
                     }
                 }
+            }
+        }
+
+        Node(vert, out var hotbar)
+            .SizeInnerSumRelativeV((1, 0))
+            .SizeInnerMaxRelativeV((0, 1))
+            .InnerSpacingV(12)
+            .InnerLayoutV(InnerLayout.HorizontalList);
+        {
+            for (int x = 0; x < HotBarSlots.Count; x++)
+            {
+                Node(hotbar, out var square)
+                    .Mut(s.Button)
+                    .SizeV((120, 120));
             }
         }
 
