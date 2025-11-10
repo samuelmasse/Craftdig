86915ebc47c36b31925d3cdedfc1eef5942f7a8c
Samuel Massé <samuelmasse4@gmail.com>
2025-11-09 19:21:00 -0800

    pem support

--- src/Crafthoe.Server/Actions/ServerLoadCertificateAction.cs
+++ src/Crafthoe.Server/Actions/ServerLoadCertificateAction.cs
@@ -5,11 +5,30 @@ public class ServerLoadCertificateAction(ServerCreateDevCertificateAction create
 {
     public X509Certificate2 Run()
     {
-        var path = Environment.GetEnvironmentVariable("CRAFTHOE_CERT_PATH");
+        var certPath = Environment.GetEnvironmentVariable("CRAFTHOE_CERT_PATH");
+        var keyPath = Environment.GetEnvironmentVariable("CRAFTHOE_CERT_KEY_PATH");
         var password = Environment.GetEnvironmentVariable("CRAFTHOE_CERT_PASSWORD");
 
-        if (path == null || password == null)
+        if (password != null)
+        {
+            if (certPath == null)
+                throw new Exception("TLS password defined without cert path");
+
+            return X509CertificateLoader.LoadPkcs12FromFile(certPath, password);
+        }
+        else if (keyPath != null)
+        {
+            if (certPath == null)
+                throw new Exception("TLS key path defined without cert path");
+
+            return X509Certificate2.CreateFromPemFile(certPath, keyPath);
+        }
+        else
+        {
+            if (certPath != null)
+                throw new Exception("TLS cert path defined without password or key");
+
             return createDevCertificateAction.Run();
-        else return X509CertificateLoader.LoadPkcs12FromFile(path, password);
+        }
     }
 }
