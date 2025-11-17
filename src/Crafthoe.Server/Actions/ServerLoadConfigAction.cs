c66d6cdf07634ef122a621005f86526877fbd1dc
Samuel Massé <samuelmasse4@gmail.com>
2025-11-16 21:22:34 -0800

    config layers

--- src/Crafthoe.Server/Actions/ServerLoadConfigAction.cs
+++ src/Crafthoe.Server/Actions/ServerLoadConfigAction.cs
@@ -1,18 +1,34 @@
 namespace Crafthoe.Server;
 
-using Microsoft.Extensions.Configuration;
-
 [Server]
 public class ServerLoadConfigAction(ServerScope scope, ServerPaths paths)
 {
     public void Run()
     {
-        var builder = new ConfigurationBuilder()
-            .AddIniFile(Path.Join(paths.Root, "Server.ini"), true)
-            .AddJsonFile(Path.Join(paths.Root, "Server.json"), true)
-            .AddEnvironmentVariables("Crafthoe");
+        var builder = new ConfigurationBuilder();
+        string[] roots = [MachineConfigDir(), UserDir(), ExeDir(), CwdDir(), paths.Root ];
 
-        var config = builder.Build().Get<ServerConfig>() ?? new();
-        scope.Add(config);
+        foreach (var root in roots.Distinct())
+        {
+            var iniPath = Path.Join(root, "Server.ini");
+            if (File.Exists(iniPath))
+                Console.WriteLine($"Loaded config {iniPath}");
+            builder.AddIniFile(iniPath, true);
+
+            var jsonPath = Path.Join(root, "Server.json");
+            if (File.Exists(jsonPath))
+                Console.WriteLine($"Loaded config {jsonPath}");
+            builder.AddJsonFile(jsonPath, true);
+        }
+
+        builder.AddEnvironmentVariables("Crafthoe_");
+        scope.Add(builder.Build().Get<ServerConfig>() ?? new());
     }
+
+    private string MachineConfigDir() => OperatingSystem.IsWindows() ?
+        Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Crafthoe") :
+        "/etc/crafthoe";
+    private string UserDir() => Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".crafthoe");
+    private string ExeDir() => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+    private string CwdDir() => Directory.GetCurrentDirectory();
 }
