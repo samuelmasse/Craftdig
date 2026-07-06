e995d8f79029ff66227b6e48a3f9464afe07e654
Samuel Massé <samuelmasse4@gmail.com>
2026-07-06 01:23:18 -0400

    agents

--- src/Craftdig.Menus/AppLoadIconAction.cs
+++ src/Craftdig.Menus/AppLoadIconAction.cs
@@ -6,6 +6,23 @@ public class AppLoadIconAction(AppFiles files, RootPngs pngs, RootScreen screen)
     public void Run()
     {
         var image = pngs[files["Textures/Icon.png"]];
-        screen.SetIcon((Vec2u)image.Size, image.Pixels.Span);
+        screen.SetIcon((Vec2u)image.Size, FlipForWindowIcon(image));
+    }
+
+    private static Vec4u8[] FlipForWindowIcon(ImageData image)
+    {
+        var width = image.Size.X;
+        var height = image.Size.Y;
+        var source = image.Pixels.Span;
+        var pixels = new Vec4u8[source.Length];
+
+        for (var y = 0; y < height; y++)
+        {
+            var sourceRow = (height - 1 - y) * width;
+            var targetRow = y * width;
+            source.Slice(sourceRow, width).CopyTo(pixels.AsSpan(targetRow, width));
+        }
+
+        return pixels;
     }
 }
